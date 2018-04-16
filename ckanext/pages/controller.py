f4ea497dccd623617273bbcc6826ec395c333f1f
rzido <rzido@yahoo.es>
2018-04-16 13:44:04 +0200

    Update controller.py

--- ckanext/pages/controller.py
+++ ckanext/pages/controller.py
@@ -325,9 +325,12 @@ class PagesController(p.toolkit.BaseController):
 
     def _pages_list_pages(self, page_type):
         
+        log.debug('*** PAGES_LIST ******************************************************')
         desired_lang_code = pylons.request.environ['CKAN_LANG']
+        log.debug (desired_lang_code)
         
         data_dict={'org_id': None, 'page_type': page_type, 'lang': desired_lang_code}
+        log.debug (data_dict)
         if page_type == 'blog':
             data_dict['order_publish_date'] = True
         p.toolkit.c.pages_dict = p.toolkit.get_action('ckanext_pages_list')(
